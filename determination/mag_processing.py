778d87c10340443ebf17440afc35c54bb7a4fb28
Natasha Evans <54152487+natasha0801@users.noreply.github.com>
2021-12-30 21:54:16 -0800

    Update mag_processing.py

--- determination/mag_processing.py
+++ determination/mag_processing.py
@@ -13,12 +13,12 @@ mag_offset = np.array((0,0,0)) # from hardware testing
 #         m_x0, m_y0, m_z0  at t = t-1
 #         delta_t = time increment
 #         r = rotation matrix from sensor frame to body frame
-# OUTPUTS: m_out = [m_x, m_y, m_z] with gains, offsets, filtering applied
+# OUTPUTS: b_body = [m_x, m_y, m_z] magnetic field in body frame
 def mag_processing(m_x, m_y, m_z, m_x0, m_y0, m_z0, delta_t, r):
     m_current = np.array((m_x, m_y, m_z))
     m_prev = np.array((m_x0, m_y0, m_z0))
     alpha = np.exp(-1*mag_wc/delta_t) # value tbd
     m_calib = np.multiply(np.subtract(m_current, mag_gain), mag_gain) # apply offsets and gains
     m_filtered = np.add(np.multiply(m_calib, alpha), np.multiply(m_prev, 1-alpha)) # apply low-pass filter
-    m_out = r.dot(m_filtered) # apply rotation matrix
-    return m_out
+    b_body = np.dot(r,m_filtered) # apply rotation matrix
+    return b_body
