b329fd353e6d5aa3667401d13d3df6443801cf37
Natasha Evans <54152487+natasha0801@users.noreply.github.com>
2022-01-17 16:25:53 -0800

    Update igrf_mag_vector_ecef.py

--- determination/igrf_mag_vector_ecef.py
+++ determination/igrf_mag_vector_ecef.py
@@ -1,24 +1,26 @@
 # imports
-import pyIGRF, math, datetime
+import pyIGRF, datetime
 import numpy as np
 from astropy.coordinates import EarthLocation
 from astropy import units as u
 
+R_e = 6371 # mean radius of Earth
 
 #INPUTS: x_ecef: position in km in ecef frame, [x y z]
+# Note sign convention: latitude is positive in the north direction, longitude is positive in the east direction
 #INPUTS: t_jd: julian date (days since noon on Jan 1 2000)
 #OUTPUT: B_ecef: magnetic field in microtesla
 def igrf_mag_vector_ecef(x_ecef, t_jd):
     
     # convert ecef to latitude and longitude
-    xq = u.quantity.Quantity(x_ecef[1], u.km)
-    yq = u.quantity.Quantity(x_ecef[2], u.km)
-    z1 = u.quanity.Quantity(x_ecef[3], u.km)
-    loc = EarthLocation(xq,yq,zq)
+    xq = u.quantity.Quantity(x_ecef[0], u.km)
+    yq = u.quantity.Quantity(x_ecef[1], u.km)
+    z1 = u.quanity.Quantity(x_ecef[2], u.km)
+    loc = EarthLocation(x=xq,y=yq,z=zq)
     longitude, latitude, altitude = loc.to_geodetic()
     lon = longitude.value # longitude in degrees
     lat = latitude.value # latitude in degrees
-    alt = altitude.value / 1000 # altitude in km
+    alt = (altitude.value / 1000) - R_e # altitude in km
     
     # get decimal date
     delta = datetime.timedelta(days = t_jd) # convert julian date to delta in days
