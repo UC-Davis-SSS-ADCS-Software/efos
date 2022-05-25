2a248075673d7ae820b3f0be2e495362b50406ef
Natasha Evans <54152487+natasha0801@users.noreply.github.com>
2022-05-25 15:20:10 -0700

    Update mag_processing.py

--- determination/mag_processing.py
+++ determination/mag_processing.py
@@ -1,19 +1,27 @@
 # mag processing
 # imports
 import numpy as np
-
+import matplotlib.pyplot as plt
 # constants
-mag_wc = 0.40 # tbd
+mag_wc = 0.15 # tbd
 mag_gain = np.array((1.0,1.0,1.0)) # from hardware testing
 mag_offset = np.array((0,0,0)) # from hardware testing
 
-
-# INPUTS: m_x, m_y, m_z at t = t
-#         m_x0, m_y0, m_z0  at t = t-1
+# INPUTS: m_x, m_y, m_z: raw magnetomer t = t
+#         m_x0, m_y0, m_z0: filtered magnetometer data at t = t-1
 #         delta_t = time increment
 #         r = rotation matrix from sensor frame to body frame
 # OUTPUTS: b_body = [m_x, m_y, m_z] magnetic field in body frame
 def mag_processing(m_x, m_y, m_z, m_x0, m_y0, m_z0, delta_t, r):
+    try:
+        m_x = float(m_x)
+        m_y = float(m_y)
+        m_z = float(m_z)
+        m_x0 = float(m_x0)
+        m_y0 = float(m_y0)
+        m_z0 = float(m_z0)
+    except:
+        return([np.inf, np.inf, np.inf])
     m_current = np.array((m_x, m_y, m_z))
     m_prev = np.array((m_x0, m_y0, m_z0))
     alpha = np.exp(-1*mag_wc/delta_t) # value tbd
