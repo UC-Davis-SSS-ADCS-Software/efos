838b0d7eee2d2fc9bd105a8d4a1e11dae72bbc4b
Natasha Evans <natashahevans@yahoo.com>
2022-08-26 14:22:14 -0700

    added SGP4 test files

--- SGP4_orbit_propagator/sgp4_test.py
+++ SGP4_orbit_propagator/sgp4_test.py
@@ -1,15 +1,28 @@
 # Test of SGP4 Orbit Propagator
-# Compute x,y,z position and velocity for ISS at 12:50:19 on 29 June 2000
-
 from sgp4.api import Satrec
+import csv
 
-# Input orbit data using TLE (Two-line element set) format
-s = '1 25544U 98067A   19343.69339541  .00001764  00000-0  38792-4 0  9991'
-t = '2 25544  51.6439 211.2001 0007417  17.6667  85.6398 15.50103472202482'
-satellite = Satrec.twoline2rv(s, t)
-
-jd, fr = 2458827, 0.362605
-e, r, v = satellite.sgp4(jd, fr)
-
-print(r)  # True Equator Mean Equinox position (km)
-print(v)  # True Equator Mean Equinox velocity (km/s)
+outfile = open('sgp4_out.txt', 'w')
+with open('testInputsSGP4.txt') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
+    for row in reader:
+        tle1 = row[0]
+        tle2 = row[1]
+        tle1 = tle1.strip("'")
+        tle2 = tle2.strip("'")
+        try:
+            jd = float(row[2])
+            fr = float(row[3])
+        except:
+            continue
+        satellite = Satrec.twoline2rv(tle1, tle2)
+        e, r, v = satellite.sgp4(jd, fr)
+        outstr = []
+        for item in r:
+            outstr.append(str(item))
+        for item in v:
+            outstr.append(str(item))
+        line = "\t".join(row)+"\t"+"\t".join(outstr) + "\n"
+        outfile.write(line)
+outfile.close()
+print('Done')
