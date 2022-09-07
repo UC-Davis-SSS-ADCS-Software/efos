a132ba1aa680c1032212b0b6bdae1931994ae775
Natasha Evans <natashahevans@yahoo.com>
2022-09-06 18:21:23 -0700

    cleaning up software skeleton + date formatting

--- Main_ADCS.py
+++ Main_ADCS.py
@@ -1,4 +1,5 @@
 from ADCS_Constants import *
+from determination.calendar_date_to_JD import calendar_date_to_JD
 from determination.SensorProcessing import mag_processing, imu_processing
 from determination.eclipseCheck import eclipseCheck
 from determination.sunLookup import sunLookup
@@ -11,11 +12,17 @@ from control_system import Attitude_Controller
 from control_system.MRW.Wheel_Speed_Controller import wheelSpeedController
 from control_system.rotisserie import rotisserie
 
-def Main_ADCS(sunsensor_inputs, mag_inputs, angvel_inputs, epoch_time, mode, TLE1, TLE2, w_rw, delta_t):
+def Main_ADCS(sunsensor_inputs, mag_inputs, angvel_inputs, datetime, mode, TLE1, TLE2, w_rw, delta_t):
 
     # Apply filters and offsets to sensor inputs
     mag_field_body = mag_processing(mag_inputs)
     body_angvel = imu_processing(angvel_inputs)
+
+    # Define date in all formats (mostly for debug purposes -- eventually we should switch to a consistent date format)
+    # This assumes we receive the date and time as a Python datetime object
+    # TODO check with CS which format they'll input to the function
+    date_as_array = [datetime.year, datetime.month, datetime.day, datetime.hour, datetime.minute, datetime.second]
+    epoch_time = calendar_date_to_JD(date_as_array)
 
     # Ensure mode is consistent format
     mode = str(mode).upper()
@@ -61,17 +68,16 @@ def Main_ADCS(sunsensor_inputs, mag_inputs, angvel_inputs, epoch_time, mode, TLE
 
         try:
             # Calculate rotation matrices
-            R_eci2ecef = ECI_TO_ECEF(epoch_time)
+            R_eci2ecef = ECI_TO_ECEF(date_as_array)
             R_ecef2eci = np.linalg.inv(R_eci2ecef)
 
             # Calculate orbit position using TLE
-            # TODO get consistent date formats
             [pos_eci, vel_eci] = sgp4(TLE1, TLE2, epoch_time)
             pos_ecef = np.dot(R_eci2ecef, pos_eci)
 
             # Get reference vectors using sun lookup and IGRF
-            sun_pos_eci = sunLookup(epoch_time)
-            mag_vector_ecef = igrf_mag_vector_ecef(pos_ecef, epoch_time)
+            sun_pos_eci = sunLookup(datetime)
+            mag_vector_ecef = igrf_mag_vector_ecef(pos_ecef, datetime)
             mag_field_eci = np.dot(R_ecef2eci, mag_vector_ecef)
 
             # Calculate attitude using TRIAD
@@ -79,12 +85,13 @@ def Main_ADCS(sunsensor_inputs, mag_inputs, angvel_inputs, epoch_time, mode, TLE
             q_measured = triad_object.triad()
             # TODO: figure out how to use target calculation
             [q_desired, w_desired] = Target_calculation(mode,pos_eci,vel_eci)
+            w_measured = body_angvel[2] # angular velocity of body about z-axis
             controller_object = Attitude_Controller.Attitude_Control(q_desired, q_measured, w_desired, w_measured)
             des_torque = controller_object.attitude_control()
             command_vector = wheelSpeedController(des_torque, w_rw, delta_t) # TODO: get loop period or elapsed time per function call
         except:
             # Enter rotisserie mode if ADCS fails
-            w_body = body_angvel[2] # MOI of body about z-axis
+            w_body = body_angvel[2] # angular velocity of body about z-axis
             command_vector = rotisserie(J_CS, w_body, RW_MOI, w_rw)
         return command_vector
 
--- determination/IGRF/igrf_mag_vector_ecef.py
+++ determination/IGRF/igrf_mag_vector_ecef.py
@@ -8,9 +8,9 @@ R_e = 6371 # mean radius of Earth
 
 #INPUTS: x_ecef: position in km in ecef frame, [x y z]
 # Note sign convention: latitude is positive in the north direction, longitude is positive in the east direction
-#INPUTS: t_jd: julian date (days since noon on Jan 1 2000)
+#INPUTS: d: datetime
 #OUTPUT: B_ecef: magnetic field in microtesla
-def igrf_mag_vector_ecef(x_ecef, t_jd):
+def igrf_mag_vector_ecef(x_ecef, d):
     
     # convert ecef to latitude and longitude
     xq = u.quantity.Quantity(x_ecef[0], u.km)
@@ -23,8 +23,6 @@ def igrf_mag_vector_ecef(x_ecef, t_jd):
     alt = (altitude.value / 1000) - R_e # altitude in km
     
     # get decimal date
-    delta = datetime.timedelta(days = t_jd) # convert julian date to delta in days
-    d = datetime.datetime(2000,1,1,12,0,0) + delta # will depend on epoch -- currently using jd2000
     decimal_date = (float(d.strftime("%j"))-1) / 366 + float(d.strftime("%Y"))
     
     # get magnetic field in ecef frame
