8d69b63bc89ad308f9c2e1bf1811f236784eb4e9
Natasha Evans <natashahevans@yahoo.com>
2023-03-05 15:59:25 -0800

    added SPI communication

--- coilsTesting/coilsBoard/coilsTesting.py
+++ coilsTesting/coilsBoard/coilsTesting.py
@@ -1,24 +1,46 @@
 # VERY WIP
-
+# Import packages
 import busio
 import board
+import spidev
+import RPi.GPIO as GPIO
+import time
 
+# Set up I2C and SPI
 i2c = busio.I2C(board.SCL, board.SDA)
-//spi = busio.SPI(board.SCL, )
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 7629
 
-import digitalio
-dirpin = digitalio.DigitalInOut(board.GPIO17) # Chose a direction GPIO pin, need to figure out pin names
-dirpin.switch_to_output()
+# Set up direction pin
+dirpin = 17
+GPIO.setup(dirpin, GPIO.setup)
+pinmode = False
+GPIO.output(dirpin, GPIO.LOW)
+def test_extremes():
+    print("0")
+    writePot(0)
+    time.sleep(2)
 
-pinmode = False 
+    print("127")
+    writePot(127)
+    time.sleep(2)
+
+    print("255")
+    writePot(255)
+    time.sleep(2)
+
+# Write an integer as a 2-byte array to send via SPI
+def writePot(input):
+    msb = input >> 8    # most significant bit
+    lsb = input & 0xFF  # least significant bit
+    spi.xfer([msb, lsb])
 
 while True:
     test_extremes()
-
-    dirpin.value = True
-    pinmode = !pinmode
+    pinmode = not pinmode
+    if (pinmode == False):
+        GPIO.output(dirpin, GPIO.LOW)
+    else:
+        GPIO.output(dirpin, GPIO.HIGH)
     print(pinmode)
-
-
-def test_extremes():
-    # Code goes here
