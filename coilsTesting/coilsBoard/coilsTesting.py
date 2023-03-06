c5e444b65116e17b2a9fd1108105ada3f29bdb5d
Chris Andrade <coandrade@ucdavis.edu>
2023-03-05 16:31:54 -0800

    added comments to coilsTesting code. Need to figure out hexadecimal thing

--- coilsTesting/coilsBoard/coilsTesting.py
+++ coilsTesting/coilsBoard/coilsTesting.py
@@ -9,15 +9,19 @@ import time
 # Set up I2C and SPI
 i2c = busio.I2C(board.SCL, board.SDA)
 spi = spidev.SpiDev()
-spi.open(0,0)
+spi.open(0,0) 
 spi.max_speed_hz = 7629
 
 # Set up direction pin
 dirpin = 17
-GPIO.setup(dirpin, GPIO.setup)
+GPIO.setup(dirpin, GPIO.OUT)
 pinmode = False
 GPIO.output(dirpin, GPIO.LOW)
+
+# Function for testing extreme 
 def test_extremes():
+
+    # Need to convert pot values to hex (pi does not use 0-255)
     print("0")
     writePot(0)
     time.sleep(2)
@@ -30,10 +34,11 @@ def test_extremes():
     writePot(255)
     time.sleep(2)
 
-# Write an integer as a 2-byte array to send via SPI
+# Write an integer as a 2-byte array to send via SPI (range 0-255)
 def writePot(input):
-    msb = input >> 8    # most significant bit
-    lsb = input & 0xFF  # least significant bit
+    # figure out hex thing 
+    msb = input >> 8    # most significant bit (first 8 bits)
+    lsb = input & 0xFF  # least significant bit (last 8 bits) (0-255 where 0xFF is 255)
     spi.xfer([msb, lsb])
 
 while True:
