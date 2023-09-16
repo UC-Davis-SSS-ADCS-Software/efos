a925dc847e38a784698167899feecdac5e521a36
jacobtkeio <jacobtkeio@gmail.com>
2023-09-16 14:47:13 -0700

    Remove Python code from flight software :( All Python code is saved in the tag 'python-archive'

--- bdot_control/bdot_simulation/Control.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def Control(BB, pqr):
-    k = 67200
-    current = k*np.cross(pqr, BB)
-    # current saturation if > 0.158 amps
-    if np.sum(np.abs(current)) > 0.158:
-        current = current/np.linalg.norm(current)*0.158
-    return current
